4f34e36a44c89205b17a0fa67cdfb9c021eed6ba
rounak <rounakkole@gmail.com>
2024-10-06 19:44:02 +0530

    add-migration pending changes

---
+++ Mark4/Data/Migrations/20241006140138_ApplicationDbContext_pending_changes.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Mark4.Migrations
+{
+    /// <inheritdoc />
+    public partial class ApplicationDbContext_pending_changes : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "ExchangeId",
+                table: "AspNetUsers",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AspNetUsers_ExchangeId",
+                table: "AspNetUsers",
+                column: "ExchangeId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_AspNetUsers_ExchangeId",
+                table: "AspNetUsers");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "ExchangeId",
+                table: "AspNetUsers",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+        }
+    }
+}
